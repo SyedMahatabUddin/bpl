126b7470f0da9849cbe3b417671ae56a08ea56d2
SyedMahatabUddin <syedmahatabuddin930@gmail.com>
2026-05-17 00:58:07 +0600

    deploying to github pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import plugin from 'eslint-plugin-react-hooks'
 
 export default defineConfig({
+  base: "/bpl-project-react/",
   plugins: [
     react(),
     tailwindcss(),
